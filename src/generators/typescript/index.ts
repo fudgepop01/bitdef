9c84677749398d6f1d5f55bf745e1d6fad7e4f07
dar2355 <fudgepop01@gmail.com>
2018-11-12 13:36:22 -0500

    added some bdef files, implemented initial parsing and detection

--- src/generators/typescript/index.ts
+++ src/generators/typescript/index.ts
@@ -1,27 +1,111 @@
-interface ITypeNodeArgs {
-  parent: TypeNode,
+import { i8 } from './integers';
+import {BigInteger as BI} from 'jsbn'
+import { EventEmitter } from 'events';
+
+// <enums>
+
+export interface ITypeNodeArgs {
   buffer: Buffer,
-  root?: TypeNode,
-  address: number
+  parent?: TypeNode,
+  root?: TypeNode
 }
 
-class TypeNode {
+/**
+ * extends EventEmitter so that it may send events to
+ * other nodes
+ */
+export class TypeNode {
   public buffer: Buffer;
-  public address: {
-    abs?: number;
-    rel?: number;
-  } = {};
-  public children: TypeNode[] = [];
-  public parent: TypeNode;
-  public root?: TypeNode;
-  constructor(args: ITypeNodeArgs) {
-    if (!args.root) { this.root = this; }
-    this.parent = args.parent;
-    this.address.rel = args.address;
-    this.address.abs = args.address;
-    this.buffer = args.buffer;
+  public array: Array<number> | undefined;
+  public children: Array<TypeNode | RawType> = [];
+  public root: TypeNode;
+  public parent?: TypeNode;
+  public pointers: Array<RawType> = [];
+  public progress: number = 0;
+  public bits: number = 0;
+  constructor({parent, buffer, root}: ITypeNodeArgs) {
+    this.root = (root) ? root : this;
+    this.parent = (parent) ? parent : undefined;
+    this.buffer = buffer;
   }
 
-  // <getters>
-  // <setters>
+  protected get i8() { return new i8(this.buffer.slice(), this) }
+  // protected get u8() { return new u8(this.buffer.slice(), this) }
+
+  // protected get i16b() { return new i16b(this.buffer.slice(), this) }
+  // protected get u16b() { return new u16b(this.buffer.slice(), this) }
+  // protected get u16l() { return new u16l(this.buffer.slice(), this) }
+  // protected get i16l() { return new i16l(this.buffer.slice(), this) }
+
+  // protected get i24b() { return new i24b(this.buffer.slice(), this) }
+  // protected get u24b() { return new u24b(this.buffer.slice(), this) }
+  // protected get u24l() { return new u24l(this.buffer.slice(), this) }
+  // protected get i24l() { return new i24l(this.buffer.slice(), this) }
+
+  // protected get i32b() { return new i32b(this.buffer.slice(), this) }
+  // protected get u32b() { return new u32b(this.buffer.slice(), this) }
+  // protected get u32l() { return new u32l(this.buffer.slice(), this) }
+  // protected get i32l() { return new i32l(this.buffer.slice(), this) }
+
+  // protected get i64b() { return new i64b(this.buffer.slice(), this) }
+  // protected get u64b() { return new u64b(this.buffer.slice(), this) }
+  // protected get u64l() { return new u64l(this.buffer.slice(), this) }
+  // protected get i64l() { return new i64l(this.buffer.slice(), this) }
+
+  // protected get i128b() { return new i128b(this.buffer.slice(), this) }
+  // protected get u128b() { return new u128b(this.buffer.slice(), this) }
+  // protected get u128l() { return new u128l(this.buffer.slice(), this) }
+  // protected get i128l() { return new i128l(this.buffer.slice(), this) }
+
+  // protected get f16b() { return new f16b(this.buffer.slice(), this) }
+  // protected get f16l() { return new f16l(this.buffer.slice(), this) }
+
+  // protected get f32b() { return new f32b(this.buffer.slice(), this) }
+  // protected get f32l() { return new f32l(this.buffer.slice(), this) }
+
+  // protected get f64b() { return new f64b(this.buffer.slice(), this) }
+  // protected get f64l() { return new f64l(this.buffer.slice(), this) }
+
+  // protected get pointer() { return new pointer(this.buffer.slice(), this) }
+
+  // protected bitfield(size: number) { return new bitfield(this.buffer.slice(), this) }
+
+  /**
+   * automatically called when a node has finished being initialized
+   */
+  protected finalize() {
+
+  }
+
+  /**
+   * adds to the overall progress of the root's
+   * @param amount how much progress to add (in bytes)
+   */
+  public incProgress(amount: number) {
+    this.progress += amount;
+  }
+  public incBit(amount: number) {
+    this.
+    this.bits += amount;
+    this.bits %= 8;
+  }
+}
+
+export interface IRawType {
+  size: number;
+  buffer: Buffer;
+  parent: TypeNode;
+  biValue: BI;
+  value: string | number;
+}
+
+export abstract class RawType {
+  public buffer: Buffer;
+  public parent: TypeNode;
+  public biValue: BI;
+  constructor(buffer: Buffer, parent: TypeNode) {
+    this.buffer = buffer;
+    this.biValue = new BI([...buffer].map(val => val.toString(2).padStart(2, '0')).join(''), 16);
+    this.parent = parent;
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,69 @@
 import * as fs from 'fs';
 import * as moo from 'moo';
 import {promisify} from 'util';
+import * as Path from 'path';
 const readFile = promisify(fs.readFile);
+const readFileSync = fs.readFileSync;
+
+const consolidate = async (path: string) => {
+  let paths: string[] = [];
+  let root = Path.dirname(path);
+
+  const replacer = (input: string): string => {
+    let file = readFileSync(input, 'utf8');
+
+    file = file.replace(/# import (?:'(.+)'|"(.+)")/g, (fullstr, match: string) => {
+      let nextPath = Path.join(root, match);
+      if (paths.indexOf(nextPath) === -1) {
+        paths.push(nextPath);
+        return replacer(nextPath);
+      }
+      return '';
+    });
+
+    return file;
+  }
+
+  paths.push(path);
+  return replacer(path);
+}
+
+
+
 const generate = async () => {
+  let input = await consolidate(`${__dirname}/bitdef-brawllib/RSTM.bdef`);
 
-  const mainOut: string = await readFile('./src/generators/typescript/index.ts', 'utf8');
+  const mainOut: string = await readFile(`${__dirname}/generators/typescript/index.ts`, 'utf8');
 
-  const lexer = moo.compile({
+  const lexMain = moo.compile({
     WS: /[ \t]+/,
     comment: /\/\/.*?$/,
     number: /0|[1-9][0-9]*/,
     lcurly: '{',
     rcurly: '}',
-    integer: /[iu](?:8|16|24|32|40|48)[bl]/,
+    lsquare: '[',
+    rsquare: ']',
+    colon: ':',
+    integer: /[iu](?:8|(?:16|24|32|40|48)[bl])/,
     bitfield: /b[1-8]/,
     float: /f(?:16|32|64|128)[bl]/,
     char: 'char',
     keywords: ['struct', 'enum', 'get', 'set', 'ref'],
     NL: { match: /\n/, lineBreaks: true, value(str) {return '\\n'} },
-    identifier: /[A-Za-z_]+[0-9A-Za-z_]*/,
+    identifier: /[A-Za-z_]+[0-9A-Za-z_]*/
   });
 
-  lexer.reset(`
-  struct thing {
-    i32l i32test
-    u8b u8test
-    u8b u8duplicate
-    b5 bitfield
-    f64b float
-  }
-  `);
+  lexMain.reset(input);
 
   let out = "";
   const typesUsed: string[] = [];
-  for (const match of lexer) {
+  for (const match of lexMain) {
     if ( match.type && typesUsed.indexOf(match.value) === -1 && ['char', 'float', 'bitfield', 'integer'].indexOf(match.type) !== -1) {
       typesUsed.push(match.value);
     }
   }
   console.log(mainOut.replace("// <getters>", typesUsed.sort().map(type => {
-    return `get ${type}() { return new ${type}() }`;
+    return `get ${type}() { return new ${type}(this.buffer, this) }`;
   }).join('\n  ')));
 
 }
